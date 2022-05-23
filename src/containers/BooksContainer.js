fd9900760d8ede5ab339020b8e63e6e698c835c0
hvirfvel <carolinehvirfvel@hotmail.com>
2022-05-23 11:31:50 +0100

    fetch bestsellers from new your times books api

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import BooksSection from "../components/BooksSection";
 
 const BooksContainer = () => {
+
+    useEffect(() => {
+        getBestSellers();
+    }, [])
+
+    const getBestSellers = () => {
+        fetch(`https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=${process.env.REACT_APP_NEW_YORK_TIMES_BOOKS_API_KEY}`, {
+            method: "GET",
+            headers: {
+              "Accept": "application/json"
+            },
+          })
+        .then(res => res.json())
+        .then(data => console.log(data.results))
+    }
+
     return (
         <>
             <BooksSection/>
